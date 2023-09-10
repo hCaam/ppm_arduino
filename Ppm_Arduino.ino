f484311dbe3498f9a4c0178c151ce6cde0d1e8b7
Taj Hakam Ikhsan <94432199+hCaam@users.noreply.github.com>
2023-09-10 20:32:18 +0700

    Update Ppm_Arduino.ino

--- Ppm_Arduino.ino
+++ Ppm_Arduino.ino
@@ -1,9 +1,9 @@
-const int output = 2;
-unsigned long previousMillis = 0;
+const int output = 3;
+unsigned int previousMillis = 0;
 const long interval = 20;
-const int longPulse = 2;
 const int delayPulse = 500;
-const int manyPulse = 6;
+int pulseDc[] = {1000, 1200, 1400, 1600, 1800, 2000};
+int manyPulse = 6;
 
 void setup() {
   pinMode(output, OUTPUT);
@@ -11,15 +11,19 @@ void setup() {
 }
 
 void loop() {
-  
-unsigned long currentMillis = millis();
+
+unsigned int currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
     for (int pulse = 0; pulse < manyPulse; pulse++) {
-      digitalWrite(output, HIGH);
-      delay(longPulse);
-      digitalWrite(output, LOW);
-      delayMicroseconds(delayPulse);
+      Pulse(pulseDc[pulse]);
     } 
   }
+}
+
+void Pulse(int dc) {
+  digitalWrite(output, HIGH);
+  delayMicroseconds(dc);
+  digitalWrite(output, LOW);
+  delayMicroseconds(delayPulse);
 }
